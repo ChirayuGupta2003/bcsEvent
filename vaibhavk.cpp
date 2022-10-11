2167ecf0359e6c0e440603afa442d852725098a1
Vaibhavk0192 <vkapoor2_be21@thapar.edu>
2022-10-12 02:17:14 +0530

    refactor

--- vaibhavk.cpp
+++ vaibhavk.cpp
@@ -1,45 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void BubbleSort(int *&a,int n)
+void BubbleSort(int *&a, int n)
 {
     int flag;
-    for (int i=0;i<n-1;i++)
+    for (int i = 0; i < n - 1; i++)
     {
-        flag=0;
-        for(int j=0;j<n-1-i;j++)
+        flag = 0;
+        for (int j = 0; j < n - 1 - i; j++)
         {
-            if(a[j]>a[j+1])
+            if (a[j] > a[j + 1])
             {
-                swap(a[j],a[j+1]);
-                flag=1;
+                swap(a[j], a[j + 1]);
+                flag = 1;
             }
         }
-        if(flag==0)
+        if (flag == 0)
         {
             break;
         }
     }
 }
 
-
-int main(){
+int main()
+{
     int n;
-    cout<<"enter size of array"<<endl;
-    cin>>n;
-    int *a=new int[n];
-    for (int i=0;i<n;i++)
+    cout << "enter size of array" << endl;
+    cin >> n;
+    int *a = new int[n];
+    for (int i = 0; i < n; i++)
     {
-        cin>>a[i];
+        cin >> a[i];
     }
 
-    BubbleSort(a,n);
-    for (int i=0;i<n;i++)
+    BubbleSort(a, n);
+    for (int i = 0; i < n; i++)
     {
-        cout<<a[i]<<" ";
+        cout << a[i] << " ";
     }
-    
-    
+
     return 0;
-
 }
