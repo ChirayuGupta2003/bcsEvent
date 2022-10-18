6f4babb9cff5335ae74a74682d6f5ce2a633742d
Vaibhavk0192 <vkapoor2_be21@thapar.edu>
2022-10-18 19:46:14 +0530

    accept chirayu 5.0

--- vaibhavk.cpp
+++ vaibhavk.cpp
@@ -34,6 +34,11 @@ int main()
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
+        cin >> a[i];
+    }
+        cin >> a[i];
+    }
+        cin >> a[i];
     }
 
     BubbSort(a, n);
